bfb7beca44f0069888e1744e512e827528081d48
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-09-23 13:10:12 -0700

    Creates ProductTable component

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-React.render(
+/*React.render(
 	<h1>Hello world!</h1>,
 	document.getElementById('example')
+);*/
+
+var ProductTable = React.createClass({
+	render: function() {
+		return (
+			<div className="product-table">
+				I am the Product Table
+			</div>
+		);
+	}
+});
+
+React.render(
+	<ProductTable />,
+	document.getElementById('content')
 );
