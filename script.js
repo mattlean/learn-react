5c756ed465e7a6bc739e309616880cbc895a35c0
Matthew Isaac Lean <isaaclean@creatibyte.com>
2015-09-23 13:45:34 -0700

    ProductList reads in data to dynamically generate Products

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-/*React.render(
-	<h1>Hello world!</h1>,
-	document.getElementById('example')
-);*/
+var data = [
+  {name: 'Pesto Chicken', desc: 'grilled chicken, monterey jack cheese, roasted red peppers, tomato, mixed greens, pesto'},
+  {name: 'Bay Shrimp Melt', desc: 'bay shrimp salad, monterey jack cheese, tomato, avocado'},
+  {name: 'Margherita al Fresco', desc: 'mozzarella, tomato, pesto, balsamic vinaigrette'}
+];
 
-var ProductTable = React.createClass({
+var ProductList = React.createClass({
+	render: function() {
+		var productNodes = this.props.data.map(function (product) {
+			return (
+				<Product name={product.name}>
+					{product.desc}
+				</Product>
+			);
+		});
+		return (
+			<div className="product-list">
+				{productNodes}
+			</div>
+		);
+	}
+});
+
+var Product = React.createClass({
 	render: function() {
 		return (
-			<div className="product-table">
-				I am the Product Table
+			<div className="product">
+				<h2>{this.props.name}</h2>
+				<p>{this.props.children}</p>
 			</div>
 		);
 	}
 });
 
 React.render(
-	<ProductTable />,
-	document.getElementById('content')
+	<ProductList data={data}/>,
+	document.getElementById('menu')
 );
