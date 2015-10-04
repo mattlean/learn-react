90ade510b5821f257b747e014cb65d812d4e97d8
Matthew Isaac Lean <matt@mattlean.com>
2015-10-04 10:20:06 -0700

    Can display static comments

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
+var CommentBox = React.createClass({
+	render: function() {
+		return (
+			<div className="commentBox">
+				<h1>Comments</h1>
+				<CommentList />
+				<CommentForm />
+			</div>
+		);
+	}
+});
+
+var CommentList = React.createClass({
+	render: function() {
+		return (
+			<div className="commentList">
+				<Comment author="Pete Hunt">This is one comment</Comment>
+			</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function() {
+		return (
+			<div className="commentForm">
+				I am a commentform!
+			</div>
+		);
+	}
+});
+
+var Comment = React.createClass({
+	render: function() {
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>
+		);
+	}
+});
+
 React.render(
-	<h1>Hello world!</h1>,
-	document.getElementById('example')
+	<CommentBox />,
+	document.getElementById('content')
 );
